30ef5edaf4f4074979641c8c88449e9be47eea78
Leo <33529768+Leo646@users.noreply.github.com>
2021-03-08 01:24:50 -0500

    Add files via upload

---
+++ Retos_Introduction/AlgoritmoCesar.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 14 17:44:52 2020
+
+@author: leonardo
+"""
+
+def main():
+    TextoPlano = input("Introducir Mensaje: ") 
+    desplazamiento  = int(input("numero de desplazamiento: "))
+    modoSeguridad    = input("Para cifrar pulse c   ....Para descifrar puse  d ")
+   
+
+    if modoSeguridad.lower().startswith('c'):
+        modoSeguridad = "cifrar"
+    elif modoSeguridad.lower().startswith('d'):
+        modoSeguridad = "descifrar"
+
+    Textocifrado = encriptar(TextoPlano, desplazamiento, modoSeguridad)
+    if modoSeguridad ==   "cifrar":
+        print(("Mensaje Cifrado:", Textocifrado))
+    elif modoSeguridad == "descifrar":
+        print(("Mensaje Descifrado:", Textocifrado)) 
+        
+        
+def encriptar(texto, despl, modo):
+    
+    Texto= texto.upper()
+    imprimeTexto = ""
+    abecedario    = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    for letra in Texto:
+        if letra in abecedario:
+            num = abecedario.find(letra)
+            if modo ==   "cifrar":
+                num = num + despl
+            elif modo == "descifrar":
+                num = num - despl
+
+            if num >= len(abecedario):
+                num -= len(abecedario)
+            elif num < 0:
+                num += len(abecedario)
+
+            imprimeTexto += abecedario[num]
+        else:
+            imprimeTexto += letra
+    return imprimeTexto
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    main()
+    input()

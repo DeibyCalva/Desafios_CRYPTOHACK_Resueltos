ce85d0954117e2fd32398faf5bc8a87eac639587
Mario Orellana <35825633+MarioOrellana1998@users.noreply.github.com>
2021-03-20 12:16:16 -0500

    Update JWT Secrets.js

--- Retos_Crypto_on_the_web/JWT Secrets.js
+++ Retos_Crypto_on_the_web/JWT Secrets.js
@@ -1,6 +1,6 @@
 
 import jwt
-encoded = jwt.encode({'username':'mario','admin':'true'},'secret',algorithm='HS256')
+encoded = jwt.encode({'username':'PatoNinja','admin':'true'},'secret',algorithm='HS256')
 print(encoded)
 
 
